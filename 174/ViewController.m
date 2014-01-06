a69b6d50eaf321680d3b741f4a4a36b5b0f475d2
Julie0e <julie_91@hanmail.net>
2014-01-06 17:04:15 +0900

    174 완료했습니다.

--- 174/ViewController.m
+++ 174/ViewController.m
@@ -7,17 +7,55 @@
 //
 
 #import "ViewController.h"
+#define CELL_ID @"CELL_ID"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource, UITabBarDelegate>
 
 @end
 
 @implementation ViewController
+{
+    NSArray *data;
+}
 
+// 셀 선택시 로그로 확인
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"Select Row : %d", indexPath.row);
+}
+
+// 악세서리 버튼 클릭시
+- (void)tableView:(UITableView *) tableView accessoryButtonTappedForRowWithIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"Accessory Button tapped : %d", indexPath.row);
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [data count];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CELL_ID];
+    if (indexPath.row == 0) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    }
+    else if (indexPath.row ==1){
+        cell.accessoryType = UITableViewCellAccessoryDetailDisclosureButton;
+    }
+    else if (indexPath.row == 2){
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }
+    
+    cell.textLabel.text = data[indexPath.row];
+    
+    return cell;
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	data = @[@"a", @"b", @"c", @"d", @"e", @"f", @"g", @"h"];
 }
 
 - (void)didReceiveMemoryWarning
